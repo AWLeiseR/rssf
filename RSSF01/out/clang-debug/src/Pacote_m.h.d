3ce7c837cb9af776524ee02e037285cee407b537
LeiseR <alan.leiser@mblabs.com.br>
2023-12-06 07:29:04 -0300

    remove duplicate

--- RSSF01/out/clang-debug/src/Pacote_m.h.d
+++
@@ -1,7 +0,0 @@
-../../inet4.5/src/inet/common/packet/chunk/Chunk_m.cc ../../inet4.5/src/inet/common/packet/chunk/Chunk_m.h : \
-	../../inet4.5/src/inet/common/packet/chunk/Chunk.msg \
-	../../inet4.5/src\inet/common/INETDefs.msg \
-	../../inet4.5/src\inet/common/Units.msg
-../../inet4.5/src/inet/common/packet/chunk/Chunk.msg:
-../../inet4.5/src\inet/common/INETDefs.msg:
-../../inet4.5/src\inet/common/Units.msg:
--- RSSF01/src/Sensor.cc
+++
@@ -1,9 +0,0 @@
-#include "Sensor.h"
-
-Define_Module(Sensor);
-
-void Sensor::initialize(){
-}
-
-void Sensor::handleMessage(cMessage *msg){
-}
